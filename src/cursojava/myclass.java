89d20f1fd79fb32922eac4cce29a3fc12b0881c4
MarcinhoGit <marcioaraujoponto2019@hotmail.com>
2022-07-06 23:08:35 -0300

    criando classes

--- src/cursojava/myclass.java
+++ src/cursojava/myclass.java
@@ -5,6 +5,9 @@ public class myclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
 	}
 
 }

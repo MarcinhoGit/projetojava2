1088bf996b71e24987d73e61ae909a86e587906c
MarcinhoGit <marcioaraujoponto2019@hotmail.com>
2022-07-06 23:25:09 -0300

    alterando classe principal

--- src/cursojava/myclass.java
+++ src/cursojava/myclass.java
@@ -3,11 +3,9 @@ package cursojava;
 public class myclass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
+		
 	}
 
 }

040aee790edcb61cb397c505cde4f0006cd267ea
MarcinhoGit <marcioaraujoponto2019@hotmail.com>
2022-07-09 22:30:25 -0300

    atualizando codigos

--- src/cursojava/myclass.java
+++ src/cursojava/myclass.java
@@ -2,10 +2,21 @@ package cursojava;
 
 public class myclass {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  
+	{ 
+	System.out.println(" Início do teste idade");
+		int idade = 40;
 		
-		System.out.println("");
+		if (idade >= 50)
 		
+	// idade verdadeira	
+	System.out.println("A idade é igual ou maior que 50!");
+	// idade falsa 
+	else  
+	
+	System.out.println("a idade NÃO é igual a 50!");
+	System.out.println("fim do teste");
+	
 	}
 
 }
